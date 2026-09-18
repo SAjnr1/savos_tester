bf07fdc9194afbec414928c65df7accf38f0be93
SAjnr1 <solomonagbeko123@gmail.com>
2026-09-18 04:09:21 +0000

    Added about us info and image

--- React/src/pages/About/About.jsx
+++ React/src/pages/About/About.jsx
@@ -3,6 +3,8 @@ import './About.css'
 import Title from '../../Components/Title/Title'
 import Navbar from '../../Components/Navbar/Navbar'
 import about_img from '../../assets/about.png'
+import person1 from '../../assets/person1.png'
+import person2 from '../../assets/person2.png'
 import Footer from '../../Components/Footer/footer'
 import play_icon from '../../assets/red-arrow.png'
 // The image dimensions are 1194 × 1144 pixels.
@@ -16,7 +18,7 @@ const About = () => {
     </div>
        <div className="project-lead">
         <div className="profile">
-          <h3>PERSON1</h3>
+          <h3>Mansura Abdullah</h3>
           <h5>Person's Position</h5>
           <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem, 
           asperiores! Officiis nostrum hic quia placeat 
@@ -28,15 +30,15 @@ const About = () => {
           non quasi excepturi placeat exercitationem, temporibus odit a. Facere.</p>
         </div>
         <div className="profile-picture">
-          <img src={about_img} className='propic'/>
+          <img src={person2} className='propic'/>
         </div>
        </div>
        <div className="project-lead">
         <div className="profile-picture">
-          <img src={about_img} className='propic'/>
+          <img src={person1} className='propic'/>
         </div>
         <div className="profile">
-          <h3>PERSON2</h3>
+          <h3>Emmanuella Agyei Boadiwaa</h3>
           <h5>Person's Position</h5>
           <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem, 
           asperiores! Officiis nostrum hic quia placeat 
@@ -50,7 +52,7 @@ const About = () => {
        </div>
        <div className="project-lead">
         <div className="profile">
-          <h3>PERSON3</h3>
+          <h3>Rejoyce Dellor Mawumelon</h3>
           <h5>Person's Position</h5>
           <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem, 
           asperiores! Officiis nostrum hic quia placeat 
