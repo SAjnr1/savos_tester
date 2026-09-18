09aa298e5d15894b42bf730afd6d759e9020d8a0
SAjnr1 <solomonagbeko123@gmail.com>
2026-09-18 03:28:33 +0000

    Updates services image

--- React/src/Components/Campus/Campus.jsx
+++ React/src/Components/Campus/Campus.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import './Campus.css'
 import gallery_1 from '../../assets/savos-shito.png'
-import gallery_2 from '../../assets/savos-soothing-gel.png'
+import gallery_2 from '../../assets/savos-coating.png'
 import gallery_3 from '../../assets/savos_ketchup.png'
-import gallery_4 from '../../assets/savos-body-cream.png'
+import gallery_4 from '../../assets/savos-sachet.png'
 // The image dimensions are 467 × 588 pixels.
 // import white_arrow from '../../assets/white-arrow.png'
 
